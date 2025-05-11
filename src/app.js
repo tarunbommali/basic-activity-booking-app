4f24c4033bce26ada32d4976aff6ca3451c16e4e
Tarun <btaruntej143@gamail.com>
2025-05-11 16:52:55 +0530

    cors options updated

--- src/app.js
+++ src/app.js
@@ -2,14 +2,33 @@ const express = require("express");
 const app = express();
 const connectDB = require("../src/config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(cookieParser());
 app.use(express.json());
+
+
+const BACKEND_URL = "https://basic-activity-booking-app-theta.vercel.app/"
+const PORT = process.env.PORT || 3000;
 
 const authRouter = require("../src/routes/auth")
 const activityRouter = require("../src/routes/activity")
 const bookingRouter = require("../src/routes/booking")
 
+
+// Middleware to set CORS headers
+let corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://basic-activity-booking-app.netlify.app",
+    "https://basic-activity-booking-app-02p3.onrender.com",
+    "https://basic-activity-booking-app-theta.vercel.app/"
+  ],
+  credentials: true, // allow cookies if you're using them
+};
+
+
+app.use(cors(corsOptions));
 app.use("/",authRouter)
 app.use("/",activityRouter)
 app.use("/booking",bookingRouter)
@@ -18,7 +37,7 @@ app.use("/booking",bookingRouter)
 connectDB()
   .then(() => {
     console.log("MongoDB connection established successfully");
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running on http://localhost:3000");
     });
   })
